db5a7b38626d1b117f0a056790a7a6d5101e227f
Messih Grmay <mesihgrmay@yahoo.com>
2022-09-03 09:49:59 -0700

    folder structure

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Test for base_model module"""
+
+import unittest
+
+class Test_User(unittest.TestCase):
+	""""Testing BaseModel class"""
+        pass
+
+if __name__ == '__main__':
+	unittest.main()
