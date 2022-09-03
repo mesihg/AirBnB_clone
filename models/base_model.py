2740959957e5b7cd3163f62191acf4bbe1ed8e2a
Messih Grmay <mesihgrmay@yahoo.com>
2022-09-03 10:22:29 -0700

    BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -4,4 +4,37 @@
 
 class BaseModel:
     """BaseModel that defines all common attributes/methods"""
-    pass
+    def __init__(self, *args, **kwargs):
+        """A  BaseModel classs"""
+
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        value = datetime.strptime(
+                            value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+
+        self.id = uuid.uuid4()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        models.storage.new(self)
+        models.storage.save()
+
+    def __str__(self):
+        """object str representation"""
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
+                                      self.id, self.__dict__))
+
+    def save(self):
+        """updates updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """dictionary containing all keys/values of __dict__ of the instance"""
+        dct = {**self.__dict__}
+        dct['__class__'] = self.__class__.__name__
+        dct['created_at'] = dct['created_at'].isoformat()
+        dct['updated_at'] = dct['updated_at'].isoformat()
+        return dct
