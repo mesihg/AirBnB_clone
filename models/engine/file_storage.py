66128a53646497eea80a8eb1a94d34db11af2002
Messih Grmay <mesihgrmay@yahoo.com>
2022-09-04 15:12:14 -0700

    fix file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""FileStorage module"""
 import json
 import os
 from models.base_model import BaseModel
@@ -22,26 +22,31 @@ classes = {"BaseModel": BaseModel,
 
 
 class FileStorage:
+    """FileStorage class"""
     __file_path = "file.json"
     __objects = {}
 
     def __init__(self):
+        """"FileStorage class constructor"""
         pass
 
     def all(self):
+        """Returns the dictionary __objects"""
         return self.__objects
 
     def new(self, obj):
+        """update __objects with new object"""
         key = obj.__class__.__name__ + "." + obj.id
         self.__objects.update({key: obj})
 
     def save(self):
+        """Serialize __objects to json file __file_path"""
         objs = {key: val.to_dict() for key, val in self.__objects.items()}
         with open(self.__file_path, 'w') as f:
             json.dump(objs, f)
 
     def reload(self):
-
+        """Deserialize the json file back to __objects"""
         if not os.path.exists(self.__file_path):
             return
         try:
