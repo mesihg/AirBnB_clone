7740637c4aec0e0818773551caf415440127093e
Messih Grmay <mesihgrmay@yahoo.com>
2022-09-04 14:22:31 -0700

    bug fix

--- models/base_model.py
+++ models/base_model.py
@@ -18,11 +18,10 @@ class BaseModel:
                         value = datetime.strptime(
                             value, "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, value)
-        if self.id is None:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
-        else:
-            self.updated_at = datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = datetime.now()
+        self.updated_at = datetime.now()
+        models.storage.new(self)
 
     def __str__(self):
         """object str representation"""
