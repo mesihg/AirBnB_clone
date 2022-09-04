1192e30b4bde2ab2c67d60755305fc827f0578bd
Messih Grmay <mesihgrmay@yahoo.com>
2022-09-04 13:25:22 -0700

    model fix

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """BaseModel module"""
 
+from datetime import datetime
+import uuid
+import models
+
 
 class BaseModel:
     """BaseModel that defines all common attributes/methods"""
@@ -15,7 +19,7 @@ class BaseModel:
                             value, "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, value)
 
-        self.id = uuid.uuid4()
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
         models.storage.new(self)
