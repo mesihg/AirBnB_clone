bf51186dd458c59d98c44bfc0fbf54eebe4de7ca
Messih Grmay <mesihgrmay@yahoo.com>
2022-09-06 12:04:21 -0700

    bug fix

--- models/base_model.py
+++ models/base_model.py
@@ -31,7 +31,7 @@ class BaseModel:
 
     def save(self):
         """Update updated_at with the current datetime."""
-        self.updated_at = datetime.today()
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
