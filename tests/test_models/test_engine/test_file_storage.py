88e0b1ec43a75cb623cc238965194c653b330b55
Messih Grmay <mesihgrmay@yahoo.com>
2022-09-06 15:23:05 -0700

    test fix

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -31,10 +31,20 @@ class Test_file_storage(unittest.TestCase):
         """Testing model if its an instance of BaseModel"""
         self.assertIsInstance(self.model, BaseModel)
 
+    def test_storage_instance(self):
+        """Testing models.storage as an instance of FileStorage"""
+        self.assertEqual(type(models.storage), FileStorage)
+
     def test_all(self):
         """Testing all method"""
         objs = models.storage.all()
         self.assertIsNotNone(objs)
+        self.assertEqual(dict, type(objs))
+
+    def test_all_with_arg(self):
+        """Testing all method with arg"""
+        with self.assertRaises(TypeError):
+            models.storage.all(None)
 
     def test_save(self):
         """Testing save method"""
@@ -50,6 +60,36 @@ class Test_file_storage(unittest.TestCase):
         for bobjs in models.storage.all().values():
             objs = bobjs
         self.assertEqual(base.to_dict()['id'], objs.to_dict()['id'])
+
+    def test_new(self):
+        basemodel = BaseModel()
+        user = User()
+        state = State()
+        place = Place()
+        city = City()
+        amenity = Amenity()
+        review = Review()
+        models.storage.new(basemodel)
+        models.storage.new(user)
+        models.storage.new(state)
+        models.storage.new(place)
+        models.storage.new(city)
+        models.storage.new(amenity)
+        models.storage.new(review)
+        self.assertIn("BaseModel." + basemodel.id, models.storage.all().keys())
+        self.assertIn(basemodel, models.storage.all().values())
+        self.assertIn("User." + user.id, models.storage.all().keys())
+        self.assertIn(user, models.storage.all().values())
+        self.assertIn("State." + state.id, models.storage.all().keys())
+        self.assertIn(state, models.storage.all().values())
+        self.assertIn("Place." + place.id, models.storage.all().keys())
+        self.assertIn(place, models.storage.all().values())
+        self.assertIn("City." + city.id, models.storage.all().keys())
+        self.assertIn(city, models.storage.all().values())
+        self.assertIn("Amenity." + amenity.id, models.storage.all().keys())
+        self.assertIn(amenity, models.storage.all().values())
+        self.assertIn("Review." + review.id, models.storage.all().keys())
+        self.assertIn(review, models.storage.all().values())
 
 
 if __name__ == '__main__':
